216f4673d2516ba96a491971da5f28d7bf95ae36
Ngo Duc Duy <duysadboy@gmail.com>
2025-10-15 22:05:57 +0700

    Refactor HomePage component: restore imports and adjust layout styles

--- frontend/src/pages/main/HomePage.jsx
+++ frontend/src/pages/main/HomePage.jsx
@@ -1,31 +1,37 @@
-// import React from 'react'
-// import { Botmi } from "@/assets/img/home/h1_botmi.png";
-// import { Piza } from "@/assets/img/home/h1_pizza.png";
-// import { hatbot } from "@/assets/img/home/h1_shape-6-500x330.png";
-// import { xanhnam } from "@/assets/img/home/h1_shape-7.png";
-// import { cachua } from "@/assets/img/home/h1_tomato.png";
-// import { rauxanh } from "@/assets/img/home/h2_la_xanh.png";
-// import { laxanh } from "@/assets/img/home/h2_shape-4.png";
-// import { nam } from "@/assets/img/home/h2_shape-5.png";
+import React from 'react'
+import  Botmi  from "@/assets/img/home/h1_botmi.png";
+import  Piza  from "@/assets/img/home/h1_pizza.png";
+import  Hatbot  from "@/assets/img/home/h1_shape-6-500x330.png";
+import  Laxanhnam  from "@/assets/img/home/h1_shape-7.png";
+import  Cachua  from "@/assets/img/home/h1_tomato.png";
+import  Rauxanh from "@/assets/img/home/h2_la_xanh.png";
+import  Laxanh from "@/assets/img/home/h2_shape-4.png";
+import  Nam  from "@/assets/img/home/h2_shape-5.png";
 const HomePage = () => {
     return (
         <>
-            {/* elementor-background-overlay */}
-            <div className="h-auto w-full bg-[#282828] relative overflow-hidden px-[10px]">
+        {/* elementor-background-overlay */}
+        <div className="h-auto w-full bg-[#222222] relative overflow-hidden px-[10px]">
+
             {/* Dark Radial Glow Background */}
             <div
-                className="absolute inset-0 z-0 overflow-hidden"
+                className="absolute inset-0 z-[0] overflow-hidden"
                 style={{
                 backgroundImage: `radial-gradient(circle 500px at 50% 200px, #3e3e3e, transparent)`,
                 }}
-            />
-                {/* Your Content/Components */}
-            <div className={`min-h-[800px] max-w-[1300px] px-5 w-full z-[10] mx-auto text-white`}>
-                <div>
-                    <h1>
-                        UNLIMITED<br />
-                        MEDIUM <span>PIZZAS</span>
-                    </h1>
+                />
+                
+            {/* Your Content/Components */}
+                <div className={`flex flex-row min-h-[800px] max-w-[1300px] px-5 w-full z-[10] mx-auto text-white`}>
+                    {/* img nền */}
+                    <div className=' absolute right-0 h-full overflow-hidden text-center z-[0]'>
+                            <img className='object-cover h-[110%]' src={Botmi} alt="" />
+                    </div>
+                    <div className='z-1'>
+                        <h1>
+                            UNLIMITED<br />
+                            MEDIUM <span>PIZZAS</span>
+                        </h1>
                         <h2>Medium 2-topping* pizza</h2>
                         <p>*Additional charge for premium toppings. Minimum of 2 required.</p>
                         <div>
@@ -35,12 +41,14 @@ const HomePage = () => {
                                 <span>$19.99</span>
                             </h3>
                         </div>
-                </div>
-                <div>
-                    
-                </div>
+                    </div>
+                    <div className='relative z-[1]*:absolute'>
+                        <div>
+                            <img src={Hatbot} alt="" />
+                        </div>
+                    </div>
             </div>
-            </div>
+        </div>
         </>
     )
 }
